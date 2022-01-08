0d4f59b46e5c2362398e679643941e30ef984c45
Antonin Marzelle <mrzatn@Antonins-MacBook-Pro.local>
2022-01-08 21:30:04 +0100

    Refactored tests and ingredients.

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -9,37 +9,30 @@ from core.models import Tag, Ingredient
 
 from recipe import serializers
 
-class TagViewSet(viewsets.GenericViewSet, 
-                 mixins.ListModelMixin,
-                 mixins.CreateModelMixin):
-    """Manage Tags in database"""
+class BaseRecipeAttrViewSet(viewsets.GenericViewSet,
+                            mixins.ListModelMixin,
+                            mixins.CreateModelMixin):
+    """Base viewset for user owned recipe attributes"""
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
-    queryset = Tag.objects.all()
-    serializer_class = serializers.TagSerializer
     
     def get_queryset(self):
         """Return objects for the current user authenticated only"""
         return self.queryset.filter(user=self.request.user).order_by("-name") # user est dans request grace permission_classes
-
+    
     def perform_create(self, serializer):
         """Create a new tag"""
         serializer.save(user=self.request.user)
+
+
+class TagViewSet(BaseRecipeAttrViewSet):
+    """Manage Tags in database"""
+    queryset = Tag.objects.all()
+    serializer_class = serializers.TagSerializer
         
 
-class IngredientViewSet(viewsets.GenericViewSet,
-                        mixins.ListModelMixin,
-                        mixins.CreateModelMixin):
+class IngredientViewSet(BaseRecipeAttrViewSet):
     """Manage Ingredients in database"""
-    authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated,)
     queryset = Ingredient.objects.all()
     serializer_class = serializers.IngredientSerializer
     
-    def get_queryset(self):
-        """Return objects for the current user authenticated only"""
-        return self.queryset.filter(user=self.request.user).order_by("-name")    
-    
-    def perform_create(self, serializer):
-        """Create a new tag"""
-        serializer.save(user=self.request.user)
